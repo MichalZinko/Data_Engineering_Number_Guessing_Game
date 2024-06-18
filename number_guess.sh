c416ab41f8a6e31fc015eab76e89b51764101d8d
M Zienk <michalzinko@gmail.com>
2024-06-18 12:15:07 +0000

    feat:Initial_message

--- number_guess.sh
+++ number_guess.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL_GAME="psql -X --username=freecodecamp --dbname=games --tuples-only -c"
 
 #asking for name
 echo "Enter your username:" 
 read NAME
-#checking if user exists in database
-#getting info about user
-#printing welcome message
-
-#inserting user to database
-#printing welcome message
+#checking if palyer exists in database
+PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name = $NAME")
+if [[ -z $PLAYER_ID ]]
+then 
+  #inserting user to database
+  INSERT_PLAYER=$($PSQL "INSERT INTO players(name) VALUES('$NAME') | sed's/ / /'g")
+  #printing welcome message
+  echo "Welcome, $NAME! It looks like this is your first time here."
+else
+  #getting info about user
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games_info WHERE player_id = $PLAYER_ID")
+  BEST_GAME=$($PSQL "SELECT MAX(number_of_guesses) FROM games_info")
+  #printing welcome message
+  echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
