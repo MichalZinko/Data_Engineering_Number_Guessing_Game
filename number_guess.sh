258d7e2c6766804e3386cfc9c00ff28d313d5c06
M Zienk <michalzinko@gmail.com>
2024-06-18 16:54:08 +0000

    feat:inputing_game_data_into_database

--- number_guess.sh
+++ number_guess.sh
@@ -10,19 +10,19 @@ PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name = '$NAME'" | sed 's/
 if [[ -z $PLAYER_ID ]]
 then 
   #inserting user to database
-  INSERT_PLAYER=$($PSQL "INSERT INTO players(name) VALUES('$NAME')" | sed 's/^ *//g')
+  INSERT_PLAYER=$($PSQL "INSERT INTO players(name) VALUES('$NAME')")
   #printing welcome message
   echo "Welcome, $NAME! It looks like this is your first time here."
 else
   #getting info about user
   GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games_info WHERE player_id = $PLAYER_ID" | sed 's/^ *//g')
-  BEST_GAME=$($PSQL "SELECT MAX(number_of_guesses) FROM games_info" | sed 's/^ *//g')
+  BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games_info" | sed 's/^ *//g')
   #printing welcome message
   echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
   #generating secret number
-  SECRET_NUMBER=$((1 + $RANDOM %10))
+  SECRET_NUMBER=$((1 + $RANDOM %1000))
   
   # create variable for amount of tries
   NUMBER_OF_GUESSES=0
@@ -50,10 +50,14 @@ GAME () {
   then
     NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES+1))
     GAME "It's lower than that, guess again:"
+  #checking if input is correct
   elif [[ $PLAYER_NUMBER -eq $SECRET_NUMBER ]]
   then
+    PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name = '$NAME'" | sed 's/^ *//g')
     NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES+1))
+    INSERTING_GAME_SCORE=$($PSQL "INSERT INTO games_info(player_id, number_of_guesses) VALUES($PLAYER_ID, $NUMBER_OF_GUESSES)")
     echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    exit 0
   fi
 }
 
