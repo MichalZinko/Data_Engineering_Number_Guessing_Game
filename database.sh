ecbb95c67eee91e081cb7e605e4200f4dfbdfbda
M Zienk <michalzinko@gmail.com>
2024-06-19 10:00:22 +0000

    fix:changing_varibles_names

--- database.sh
+++ database.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=postgres"
-PSQL_GAME="psql -X --username=freecodecamp --dbname=games --tuples-only -c"
+PSQL_GAME="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
 
 $PSQL -lqt | cut -d \| -f 1 | grep -qw games
 GAMES_DATABASE_EXISTANCE=$?
 if [[ $GAMES_DATABASE_EXISTANCE == 0 ]]
 then 
-  DELETE_DATABASE=$($PSQL -t --no-align -c "DROP DATABASE games")
+  DELETE_DATABASE=$($PSQL -t --no-align -c "DROP DATABASE number_guess")
 fi
 #palyer_id
 #player_name
 #amount of games
 #number of guesses 
 
-CREATING_DATABASE=$($PSQL -t --no-align -c "CREATE DATABASE games")
+CREATING_DATABASE=$($PSQL -t --no-align -c "CREATE DATABASE number_guess")
 CREATING_PLAYER_TABLE=$($PSQL_GAME "CREATE TABLE players(player_id SERIAL PRIMARY KEY, name VARCHAR(22))")
 CREATING_GAMES_TABLE=$($PSQL_GAME "CREATE TABLE games_info(game_id SERIAL PRIMARY KEY, player_id INT, number_of_guesses INT, FOREIGN KEY (player_id) REFERENCES players(player_id))")
 
--- number_guess.sh
+++ number_guess.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-PSQL="psql -X --username=freecodecamp --dbname=games --tuples-only -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 #asking for name
-echo "Enter your username:" 
-read NAME
+echo -e "\nEnter your username:" 
+read USERNAME
 #checking if palyer exists in database
-PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name = '$NAME'" | sed 's/^ *//g')
-if [[ -z $PLAYER_ID ]]
+PLAYER_NAME=$($PSQL "SELECT name FROM players WHERE name = '$USERNAME'")
+if [[ -z $PLAYER_NAME ]]
 then 
   #inserting user to database
-  INSERT_PLAYER=$($PSQL "INSERT INTO players(name) VALUES('$NAME')")
+  INSERT_PLAYER=$($PSQL "INSERT INTO players(name) VALUES('$USERNAME')")
   #printing welcome message
-  echo "Welcome, $NAME! It looks like this is your first time here."
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
 else
   #getting info about user
-  GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games_info WHERE player_id = $PLAYER_ID" | sed 's/^ *//g')
-  BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games_info" | sed 's/^ *//g')
+  PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name = '$USERNAME'")
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games_info WHERE player_id = $PLAYER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games_info")
   #printing welcome message
-  echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
   #generating secret number
   SECRET_NUMBER=$((1 + $RANDOM %1000))
   
   # create variable for amount of tries
-  NUMBER_OF_GUESSES=0
+  NUMBER_OF_GUESSES=1
 
 GAME () {
   if [[ $1 ]]
@@ -54,7 +55,6 @@ GAME () {
   elif [[ $PLAYER_NUMBER -eq $SECRET_NUMBER ]]
   then
     PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name = '$NAME'" | sed 's/^ *//g')
-    NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES+1))
     INSERTING_GAME_SCORE=$($PSQL "INSERT INTO games_info(player_id, number_of_guesses) VALUES($PLAYER_ID, $NUMBER_OF_GUESSES)")
     echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
     exit 0
